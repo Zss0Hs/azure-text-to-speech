2cfeda641e7a964d2b6c5bacf199fffab5aeaac2
lijungang <zss0hs@gmail.com>
2024-04-13 00:30:21 +0800

    修改GitHub Actions，使用Wrangler部署worker

--- copy.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-fs.copyFile('worker.js', 'dist/worker.js', (err) => {
-  if (err) throw err;
-  console.log('worker.js was copied to dist/worker.js');
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './worker.js', // 你的 Cloudflare Workers 代码文件
+  entry: './worker.js', // 您的 Cloudflare Workers 代码文件
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js', // 打包后的文件名
+    filename: 'worker.js', // 打包后的文件名保持与入口文件相同
     libraryTarget: 'umd', // 设置为 'umd' 以兼容 CommonJS、AMD 和全局变量导入
   },
   mode: 'production', // 生产模式
